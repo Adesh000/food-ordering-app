98432a9a65716c648787d6b36c3f5331d623b0ad
Adesh Nayak <adeshnayak6@gmail.com>
2022-11-11 17:14:44 +0530

    Project Completed

--- src/Components/Deals.js
+++ src/Components/Deals.js
@@ -3,16 +3,23 @@ import filterIcon from "../Images/filterIcon.svg";
 import Cards from "./Cards";
 import Menu from "./Menu";
 
-const Deals = () => {
+const allCategories = [...new Set(Menu.map((element) => element.category)), "All"];
 
-    const [items, setItems] = useState(Menu)
+const Deals = () => {
+    const [items, setItems] = useState(Menu);
+    const [categoryItems, setCategoryItems] = useState(allCategories);
 
     const filterMenu = (value) => {
+        if(value === 'All') {
+            setItems(Menu);
+            return;
+        }
+
         const updateItems = Menu.filter((elem) => {
             return elem.category === value;
-        })
+        });
         setItems(updateItems);
-    }
+    };
 
     return (
         <>
@@ -30,32 +37,27 @@ const Deals = () => {
                 </div>
             </div>
             <div className="mx-28 my-12">
-                <button className="bg-red-100 rounded-3xl px-5 py-1 mx-3 hover:text-orange-500">
-                    All
-                </button>
-                <button className="bg-red-100 rounded-3xl px-5 py-1 mx-3 hover:text-orange-500" onClick={() => filterMenu('Break Fast')} >
-                    Break Fast
-                </button>
-                <button className="bg-red-100 rounded-3xl px-5 py-1 mx-3 hover:text-orange-500" onClick={() => filterMenu('Lunch')}>
-                    Lunch
-                </button>
-                <button className="bg-red-100 rounded-3xl px-5 py-1 mx-3 hover:text-orange-500" onClick={() => filterMenu('Dinner')}>
-                    Dinner
-                </button>
-                <button className="bg-red-100 rounded-3xl px-5 py-1 mx-3 hover:text-orange-500" onClick={() => filterMenu('Desserts')}>
-                    Desserts
-                </button>
-                <button className="bg-red-100 rounded-3xl px-5 py-1 mx-3 hover:text-orange-500" onClick={() => filterMenu('Fastfood')}>
-                    Fastfood
-                </button>
+                {categoryItems.map((element, index) => {
+                    return (
+                        <button
+                            className="bg-red-100 rounded-3xl px-5 py-1 mx-3 hover:text-orange-500"
+                            key={index}
+                            onClick={() => filterMenu(element)}
+                        >
+                            {element}
+                        </button>
+                    );
+                })}
             </div>
 
             {/* Cards */}
-            <h1 className="text-3xl font-bold ml-32">Discover the best food & drinks in :-</h1>
+            <h1 className="text-3xl font-bold ml-32">
+                Discover the best food & drinks in :-
+            </h1>
             <div className="grid grid-cols-4 gap-5 m-32">
-                {
-                    items.map(element => <Cards mealData={element}/>)
-                }
+                {items.map((element) => (
+                    <Cards mealData={element} />
+                ))}
             </div>
         </>
     );
